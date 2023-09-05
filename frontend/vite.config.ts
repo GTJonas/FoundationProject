c7171447725b9194bdd18b2e492b6d7518f19014
GTJonas <143603242+GTJonas@users.noreply.github.com>
2023-09-05 15:40:19 +0200

    Initial

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+
+export default {
+  server: {
+    host: 'localhost', // Change this to your desired host
+    port: 3000,        // The port your development server listens on
+  },
+  // Other Vite configuration options
+}
